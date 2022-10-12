0a006cd335fb0a42755fb7db9aeaf925ad9118e3
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2022-10-12 13:47:50 +0200

    cteated new 10 dice and dice state

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -1,6 +1,26 @@
+import { useState } from 'react';
+import Die from './Die';
+import { nanoid } from 'nanoid';
 import './App.css';
 
 function App() {
+	const [dice, setDice] = useState(createNewDice());
+
+	function createNewDice() {
+		const diceArr = [];
+		for (let i = 0; i < 10; i++) {
+			const newDie = {
+				id: nanoid(),
+				value: Math.ceil(Math.random() * 6),
+				isHeld: false,
+			};
+			diceArr.push(newDie);
+		}
+		return diceArr;
+	}
+
+	const diceList = dice.map((die) => <Die key={die.id} value={die.value} isHeld={die.isHeld} />);
+
 	return (
 		<main style={{ margin: '0 auto' }}>
 			<h1 className="title">Tenzies</h1>
@@ -8,7 +28,7 @@ function App() {
 				Roll until all dice are the same. Click each die to freeze it at its current value between
 				rolls.
 			</p>
-			<div className="dice-container"></div>
+			<div className="dice-container">{diceList}</div>
 			<button className="roll-dice">Roll</button>
 		</main>
 	);
--- tenzies-game/src/Die.js
+++ tenzies-game/src/Die.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Die() {
+export default function Die(props) {
 	return (
 		<div className="die-face">
-			<h2 className="die-num"></h2>
+			<h2 className="die-num">{props.value}</h2>
 		</div>
 	);
 }
