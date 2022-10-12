5cf1f8f2dbc948d7412f9df84cd5ef9ce7d15b87
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2022-10-12 14:58:10 +0200

    added tracking number of rolls

--- tenzies-game/src/App.js
+++ tenzies-game/src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 function App() {
 	const [dice, setDice] = useState(createNewDice());
 	const [tenzies, setTenzies] = useState(false);
+	const [rolls, setRolls] = useState(10);
 
 	useEffect(() => {
 		const firstDie = dice[0].value;
@@ -57,9 +58,14 @@ function App() {
 					}
 				}),
 			);
+			setRolls(rolls - 1);
+			if (rolls <= 1) {
+				setTenzies(true);
+			}
 		} else {
 			setTenzies(false);
 			setDice(createNewDice());
+			setRolls(10);
 		}
 	}
 
@@ -74,8 +80,9 @@ function App() {
 
 	return (
 		<main style={{ margin: '0 auto' }}>
-			{tenzies && <Confetti />}
+			{tenzies && rolls > 0 && <Confetti />}
 			<h1 className="title">Tenzies</h1>
+			<h4>{rolls > 0 ? `You have ${rolls} left.` : 'Sorry you run out your roll!'}</h4>
 			<p className="instructions">
 				Roll until all dice are the same. Click each die to freeze it at its current value between
 				rolls.
